068e6681b3535448a15a388c52ed07609b9f53bf
John Winborne <michwinborne@gmail.com>
2026-01-19 18:47:31 -0800

    Fix HW1 issues

--- hw1/01-fizzbuzz.js
+++ hw1/01-fizzbuzz.js
@@ -7,12 +7,9 @@ Write a program that writes all the numbers from 1 to 100, with some exceptions:
 
 Use console.log() to write the proper output to the command line.
 
-
-
 **/
-
 for (let i = 1; i <= 100; i++) {
-  if (i % 3 === 0 && i % 5 === 0) {
+  if (i % 15 === 0) {
     console.log("fizzbuzz");
   } else if (i % 3 === 0) {
     console.log("fizz");
@@ -22,6 +19,9 @@ for (let i = 1; i <= 100; i++) {
     console.log(i);
   }
 }
+
+
+
 
 
 // 1
